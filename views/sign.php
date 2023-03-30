f07e95a41dd901b63be97945c912623b17f69764
irsyadagungh <irsyadagung08@gmail.com>
2023-03-30 22:19:13 +0700

    form

--- views/sign.php
+++ views/sign.php
@@ -16,61 +16,62 @@
         <h2 class="welcome">Welcome to T-Zens</h2>
         <p class="sign">SIGN UP</p>
         <div class="done">
+            <form action="" method="post">
+                <!-- Kolom 1 -->
+                <div class="kolom1">
+                    <input class="depan" name="nim">
+                </div>
+                <div class="inputan">
+                    <p>NIM :</p>
+                </div>
 
-            <!-- Kolom 1 -->
-            <div class="kolom1">
-                <input class="depan">
-            </div>
-            <div class="inputan">
-                <p>Nama Depan :</p>
-            </div>
+                <!-- Kolom 2 -->
+                <div class="kolom2">
+                    <input class="belakang">
+                </div>
+                <div class="inputan2">
+                    <p>Nama :</p>
+                </div>
 
-            <!-- Kolom 2 -->
-            <div class="kolom2">
-                <input class="belakang">
-            </div>
-            <div class="inputan2">
-                <p>Nama Belakang :</p>
-            </div>
+                <!-- Kolom 3 -->
+                <div class="kolom3">
+                    <input class="tengah">
+                </div>
+                <div class="inputan3">
+                    <p>Email :</p>
+                </div>
 
-            <!-- Kolom 3 -->
-            <div class="kolom3">
-                <input class="tengah">
-            </div>
-            <div class="inputan3">
-                <p>Email :</p>
-            </div>
+                <!-- Kolom 4 -->
+                <div class="kolom4">
+                    <input class="bawah">
+                </div>
+                <div class="inputan4">
+                    <p>Password :</p>
+                </div>
 
-            <!-- Kolom 4 -->
-            <div class="kolom4">
-                <input class="bawah">
-            </div>
-            <div class="inputan4">
-                <p>Password :</p>
-            </div>
+                <div class="checkbox">
+                    <input type="checkbox">
+                </div>
 
-            <div class="checkbox">
-                <input type="checkbox">
-            </div>
+                <div class="lisensi">
+                    <p>Saya menyetujui <a href=""> kebijakan kebijakan </a> yang telah dibuat.</p>
+                </div>
+                <div class="memiliki">
+                    <p><a href="login.php"> Sudah memiliki Akun? </a></p>
+                </div>
+                <div class="lisensi2">
+                    <p>By clicking “Sign up” button, you are creating a T-Zens account, and you agree to T.Z ens <a
+                            href="">
+                            Terms of Use </a> and <a href=""> Privacy Policy.</a></p>
+                </div>
+                <div class="img">
+                    <img src="pics/duduk.png" alt="" width="200px" height="">
+                </div>
 
-            <div class="lisensi">
-                <p>Saya menyetujui <a href=""> kebijakan kebijakan </a> yang telah dibuat.</p>
-            </div>
-            <div class="memiliki">
-                <p><a href="login.php"> Sudah memiliki Akun? </a></p>
-            </div>
-            <div class="lisensi2">
-                <p>By clicking “Sign up” button, you are creating a T-Zens account, and you agree to T.Z ens <a href="">
-                        Terms of Use </a> and <a href=""> Privacy Policy.</a></p>
-            </div>
-            <div class="img">
-                <img src="pics/duduk.png" alt="" width="200px" height="">
-            </div>
+                <button class="button button2">Daftar</button>
 
-            <button class="button button2">Daftar</button>
-
-            <button class="button button2" onclick="window.location.href='done.html'">Daftar</button>
-
+                <button class="button button2" onclick="window.location.href='done.html'">Daftar</button>
+            </form>
 
         </div>
     </div>
